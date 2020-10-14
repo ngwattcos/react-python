a5bca39c714c9c14aaccb8aef9de57eabfe9cf97
Scott Wang <curiosity142000@gmail.com>
2020-10-14 14:40:35 -0400

    working creating container app scripts

--- scripts/create-app-container.js
+++ scripts/create-app-container.js
@@ -1,14 +1,19 @@
-const fs = require('rs');
+#!/usr/bin/env node
+
+const fs = require('fs');
 // const { exec } = require('child_process');
 
 const containerAppName = 'app-container';
 
+
+// Use this in the future, since this will become a module
+// const appDir = `./../${containerAppName}`;
 const appDir = `./${containerAppName}`;
 
 if (fs.existsSync(appDir)) {
-  console.log('Directory exists!');
+  console.log('Directory exists! Finishing.');
 } else {
-  console.log('Directory not found.');
+  console.log('Directory not found. Creating app container.');
   // exec(`npx create-react-app ${containerAppName} && npm start`);
 
 }
