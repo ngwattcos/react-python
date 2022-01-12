0ec9b74960a0f5c8f4c6d49df70169ceda69cd9d
Scott Wang <curiosity142000@gmail.com>
2022-01-12 15:58:17 -0500

    factor out repeated code in compiler.js

--- bin/compiler.js
+++ bin/compiler.js
@@ -2,46 +2,42 @@
  * Invokes the transpiler on a single file
  */
 
-const shell = require('shelljs');
-const files = require('./files');
+const shell = require("shelljs");
+const files = require("./files");
 
 const versionOfOs = {
-    'linux': 'pyxyc-linux.byte',
-    'darwin': 'pyxyc-darwin.byte',
-}
+  linux: "pyxyc-linux.byte",
+  darwin: "pyxyc-darwin.byte",
+};
 
-let _compiler = '';
+let _compiler = "";
 
-let _version = '';
+let _version = "";
 
-const setup = ({os, version}) => {
-    _compiler = versionOfOs[os];
-    _version = version;
-}
+const setup = ({ os, version }) => {
+  _compiler = versionOfOs[os];
+  _version = version;
+};
 
 const supportedOs = (os) => {
-    return os in versionOfOs;
-}
+  return os in versionOfOs;
+};
 
 const transpile = (inDir, outDir, path) => {
-    if (shell.exec(`${__dirname}/../${_compiler} ${path} ${inDir} ${outDir}`).code !== 0) {
-        shell.echo(`Error in compiling ${path}`);
-        shell.exit(1);
-    }
-}
-
-
-const transpilePersistent = (inDir, outDir, path) => {
-    if (shell.exec(`${__dirname}/../${_compiler} ${path} ${inDir} ${outDir}`).code !== 0) {
-        shell.echo(`Error in compiling ${path}`);
-    }
-}
+  if (
+    shell.exec(`${__dirname}/../${_compiler} ${path} ${inDir} ${outDir}`)
+      .code !== 0
+  ) {
+    shell.echo(`Error in compiling ${path}`);
+    shell.exit(1);
+  }
+};
 
 const addDirectory = (outDir, path) => {
-    if (shell.exec(`mkdir -p ${outDir}${path}`).code !== 0) {
-        shell.echo(`Error in creating directory ${path}`);
-        shell.exit(1);
-    }
-}
+  if (shell.exec(`mkdir -p ${outDir}${path}`).code !== 0) {
+    shell.echo(`Error in creating directory ${path}`);
+    shell.exit(1);
+  }
+};
 
-module.exports = { transpile, transpilePersistent, addDirectory, setup, supportedOs };
+module.exports = { transpile, addDirectory, setup, supportedOs };
--- bin/watch-and-compile.js
+++ bin/watch-and-compile.js
@@ -3,7 +3,7 @@
 /**
  * This file will call an instance of 'chokidar'
  * to watch an input directory for changes
- * 
+ *
  */
 
 const fs = require("fs");
@@ -13,64 +13,65 @@ const files = require("./files");
 
 compiler.setup({
   os: process.platform,
-  version: "0.1"
-})
+  version: "0.1",
+});
 
 const optionalSlash = (dir) => {
   return dir.substring(dir.length - 1, dir.length) !== "/" ? "/" : "";
-}
+};
 
 new Promise((resolve, reject) => {
-  let rawConfig = fs.readFileSync('react-python-config.json');
+  let rawConfig = fs.readFileSync("react-python-config.json");
   let configData = JSON.parse(rawConfig);
   resolve(configData);
-}).then(configData => {
-  // should be read from json
-  if (!files.directoryExists(`${configData.inDir}`)) {
-    throw new Error("invalid input dir, run react-python-setup again");
-  }
-  if (!files.directoryExists(`${configData.inDir}`)) {
-    throw new Error("invalid output dir, run react-python-setup again");
-  }
-  return configData;
-}).then(configData => {
-  console.log(`input directory: ${configData.inDir}`);
-  console.log(`output directory: ${configData.outDir}`);
+})
+  .then((configData) => {
+    // should be read from json
+    if (!files.directoryExists(`${configData.inDir}`)) {
+      throw new Error("invalid input dir, run react-python-setup again");
+    }
+    if (!files.directoryExists(`${configData.inDir}`)) {
+      throw new Error("invalid output dir, run react-python-setup again");
+    }
+    return configData;
+  })
+  .then((configData) => {
+    console.log(`input directory: ${configData.inDir}`);
+    console.log(`output directory: ${configData.outDir}`);
 
-  const inDir = configData.inDir + optionalSlash(configData.inDir);
-  const outDir = configData.outDir + optionalSlash(configData.outDir);
+    const inDir = configData.inDir + optionalSlash(configData.inDir);
+    const outDir = configData.outDir + optionalSlash(configData.outDir);
 
-  if (inDir === outDir) {
-    // avoid cycles
-    throw new Error("Input and output directories are the same")
-  }
+    if (inDir === outDir) {
+      // avoid cycles
+      throw new Error("Input and output directories are the same");
+    }
 
-  if (compiler.supportedOs(process.platform)) {
-    chokidar.watch(`${configData.inDir}`).on('all', function(event, path) {
+    if (compiler.supportedOs(process.platform)) {
+      chokidar.watch(`${configData.inDir}`).on("all", function (event, path) {
+        const inPattern = `${inDir}`;
+        const root = `${configData.inDir}`;
 
+        if (path !== inPattern && path !== root) {
+          const pathInDir = path.replace(inPattern, "");
 
-      const inPattern = `${inDir}`;
-      const root = `${configData.inDir}`;
-      
-      if (path !== inPattern && path !== root) {
-        const pathInDir = path.replace(inPattern, "");
-  
-        console.log(`${event}: (${inDir}) ${pathInDir}`);
-  
-        if (event === "add" || event === "change") { 
-          compiler.transpilePersistent(inDir, outDir, pathInDir);
-        } else if (event === "addDir") {
-          compiler.addDirectory(outDir, pathInDir);
-        } else if (event === "unlink") {
-          console.log("Sorry, deleted files and directories are coming soon!");
+          console.log(`${event}: (${inDir}) ${pathInDir}`);
+
+          if (event === "add" || event === "change") {
+            compiler.transpile(inDir, outDir, pathInDir);
+          } else if (event === "addDir") {
+            compiler.addDirectory(outDir, pathInDir);
+          } else if (event === "unlink") {
+            console.log(
+              "Sorry, deleted files and directories are coming soon!"
+            );
+          }
         }
-      }
-    });
-  } else {
-    throw new Error("Unsupported OS: " + process.platform0);
-  }
-
-  
-}).catch(err => {
-  console.log(err);
-});
+      });
+    } else {
+      throw new Error("Unsupported OS: " + process.platform0);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
